c57e2add77011d7562947e837e94739a0e6940fb
leberSmeaton <77179232+leberSmeaton@users.noreply.github.com>
2021-06-28 12:27:27 +1000

    created show for band

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -2,4 +2,10 @@ class BandsController < ApplicationController
     def index
         @bands = Band.all.includes(:genre)
     end
+
+    def show
+        @band = Band.find(params[:id])
+        puts "*****"
+        pp @band
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root "pages#home"
   
   get "bands", to: "bands#index", as: "bands"
+  get "bands/:id", to: "bands#show", as: "band"
 end
