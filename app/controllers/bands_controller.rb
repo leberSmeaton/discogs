416cb03a56cff5120f7512f96ecce0c359117ef7
leberSmeaton <77179232+leberSmeaton@users.noreply.github.com>
2021-06-29 11:13:18 +1000

    added destroy method and private method and before action to bandscontroller

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -1,11 +1,46 @@
 class BandsController < ApplicationController
+    skip_before_action :verify_authenticity_token
+    before_action :set_band, only: [:show, :destroy]
+
     def index
         @bands = Band.all.includes(:genre)
     end
 
     def show
-        @band = Band.find(params[:id])
-        puts "*****"
-        pp @band
+        # don't need anything here...
+        # because we've set the private method set_band and called it's action in the before_action uptop
+        # # @band = Band.find(params[:id])
+        # puts "*****"
+        # pp @band
+    end
+
+    def new
+        @genres = Genre.all
+    end
+
+    def create
+        @band = Band.new(band_params)
+        @band.save
+        redirect_to bands_path
+    end
+
+    def destroy
+        # @band = Band.find(params[:id]) 
+        # don't need because we've set the private method set_band and called it's action in the before_action uptop
+        @band.destroy
+        redirect_to bands_path
+    end
+
+    private
+    def band_params
+        params.require(:band).permit(:name, :genre_id, :description)
+    end
+
+    def set_band
+        begin
+            @band = Band.find(params[:id])    
+        rescue 
+            render "not_found"
+        end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   root "pages#home"
   
   get "bands", to: "bands#index", as: "bands"
+  post "bands", to: "bands#create"
+  get "bands/new", to: "bands#new", as: "new_bands"
   get "bands/:id", to: "bands#show", as: "band"
+  delete "bands/:id", to: "bands#destroy" #using the same prefix as "band", but you don't need to include it.
 end
